2a0334c95a6cf7774190da63c7088c8b3301a7c7
PadNim14 <npadmana@purdue.edu>
2023-03-22 20:02:58 -0400

    first attempt at firewall

--- HW9/firewall404.sh
+++ HW9/firewall404.sh
@@ -14,24 +14,38 @@ sudo iptables -P INPUT REJECT
 sudo iptables -P FORWARD REJECT
 sudo iptables -P OUTPUT REJECT
 
-# For all outgoing packets, change source IP address to local machine IP
-sudo iptables -t nat -A POSTROUTING --destination 128.46.4.36 -j SNAT --to-source 128.210.107.85
+# Only accept packets that originate from f1.com
+sudo iptables -A INPUT -p tcp --dport 80 -s f1.com -j ACCEPT
 
-# Protect against nonstop scanning of ports 
+# Change source IP address to local machine IP address
+sudo iptables -t nat -n -A POSTROUTING -o wlp59s0 -j MASQUERADE
 
+# Protect against indiscriminate and nonstop scanning of ports 
+sudo iptables -A FORWARD -p tcp --tcp-flags SYN,ACK,FIN,RST \
+-m limit --limit 1/s -j ACCEPT
 
 # Protect from SYN-flood attack (limit new conns to 1 per second once local machine has reached 500 requests.)
-
+sudo iptables -A FORWARD -p tcp --syn -m limit --limit 1/s -j ACCEPT
 
 # Allow full loopback access on your machine
+sudo iptables -A INPUT -i lo -j ACCEPT
+sudo iptables -A OUTPUT -i lo -j ACCEPT
 
 
 # Port forwarding rule routing all traffic on port 8888 to port 25565
-
+sudo iptables -t nat -A PREROUTING -p tcp -dport 8888 -j \
+DNAT --to-destination :25565
 
 # Only allows outgoing ssh connections to engineering.purdue.edu
+sudo iptables -A -p tcp --dport 22 -d engineering.purdue.edu -m \
+state --state NEW, ESTABLISHED -j ACCEPT
 
+sudo iptables -A -p tcp --sport 22 -s engineering.purdue.edu -m \
+state --state NEW, ESTABLISHED -j ACCEPT 
 
 # Drop any other packets if not caught by above rules
+sudo iptables -A INPUT -j DROP
+sudo iptables -A FORWARD -j DROP
+sudo iptables -A OUTPUT -j DROP
 
 
