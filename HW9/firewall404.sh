3073dfb9ea402fd836481bba443b3f85872d4672
PadNim14 <npadmana@purdue.edu>
2023-03-27 14:53:27 -0400

    i think it works???

--- HW9/firewall404.sh
+++ HW9/firewall404.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # Flush and delete all previously defined rules and chains
 sudo iptables -t filter -F
 sudo iptables -t filter -X
@@ -8,40 +9,32 @@ sudo iptables -t nat -X
 sudo iptables -t raw -F
 sudo iptables -t raw -X
 
-# Default policy for INPUT/FORWARD/OUTPUT on filter: ACCEPT
-# Change default policies for all 3 chains to REJECT
-sudo iptables -P INPUT REJECT
-sudo iptables -P FORWARD REJECT
-sudo iptables -P OUTPUT REJECT
-
 # Only accept packets that originate from f1.com
 sudo iptables -A INPUT -p tcp --dport 80 -s f1.com -j ACCEPT
 
 # Change source IP address to local machine IP address
-sudo iptables -t nat -n -A POSTROUTING -o wlp59s0 -j MASQUERADE
+sudo iptables -t nat -A POSTROUTING -o wlp59s0 -j MASQUERADE
 
 # Protect against indiscriminate and nonstop scanning of ports 
-sudo iptables -A FORWARD -p tcp --tcp-flags SYN,ACK,FIN,RST \
--m limit --limit 1/s -j ACCEPT
+sudo iptables -A FORWARD -p tcp --tcp-flags SYN,ACK,FIN,RST NONE -m limit --limit 1/s -j ACCEPT
 
 # Protect from SYN-flood attack (limit new conns to 1 per second once local machine has reached 500 requests.)
 sudo iptables -A FORWARD -p tcp --syn -m limit --limit 1/s -j ACCEPT
 
 # Allow full loopback access on your machine
 sudo iptables -A INPUT -i lo -j ACCEPT
-sudo iptables -A OUTPUT -i lo -j ACCEPT
-
+sudo iptables -A OUTPUT -o lo -j ACCEPT
 
 # Port forwarding rule routing all traffic on port 8888 to port 25565
-sudo iptables -t nat -A PREROUTING -p tcp -dport 8888 -j \
-DNAT --to-destination :25565
+sudo iptables -t nat -A PREROUTING -p tcp --dport 8888 -j DNAT --to-destination :25565
+
 
 # Only allows outgoing ssh connections to engineering.purdue.edu
-sudo iptables -A -p tcp --dport 22 -d engineering.purdue.edu -m \
-state --state NEW, ESTABLISHED -j ACCEPT
+sudo iptables -A OUTPUT -p tcp --dport 22 -d engineering.purdue.edu -m \
+state --state NEW,ESTABLISHED -j ACCEPT
 
-sudo iptables -A -p tcp --sport 22 -s engineering.purdue.edu -m \
-state --state NEW, ESTABLISHED -j ACCEPT 
+sudo iptables -A INPUT -p tcp --sport 22 -s engineering.purdue.edu -m \
+state --state ESTABLISHED -j ACCEPT 
 
 # Drop any other packets if not caught by above rules
 sudo iptables -A INPUT -j DROP
